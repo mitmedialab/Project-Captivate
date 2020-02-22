c3309feeba35fdb31d12477d881b6c4e6c79adef
Patrick <chwalek@mit.edu>
2020-02-22 12:59:43 -0500

    removed files

--- stm32_mainMCU_Code_v2/Src/start.cpp
+++
@@ -1,9 +0,0 @@
-// http://blog.atollic.com/best-practices-faq-truestudio-stm32cubemx#cpp
-
-extern "C" int start_cpp() {
-
-  /* Starting point for all C++ code */
-
-  while(1); /* Assuming you don't wish to return to main() in main.c */
-
-}
